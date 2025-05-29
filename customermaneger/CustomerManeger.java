6783c0b919a94723986abb216cfc8bb5b837d45e
bahattinyunuscetin <bahattinyunuscetin@hotmail.com>
2025-05-29 10:10:48 +0300

    upt
    up

---
+++ customermaneger/CustomerManeger.java
@@ -0,0 +1,21 @@
+// Java'da Nesne Yönelimli Programlama (OOP) mantığını anlatan bir eğitim kodu
+
+// Müşteri işlemlerini yöneten bir sınıf
+class CustomerManager {
+
+    // Müşteri ekleme işlemi
+    public void add() {
+        System.out.println("Müşteri eklendi.");
+    }
+
+    // Müşteri silme işlemi
+    public void remove() {
+        System.out.println("Müşteri silindi.");
+    }
+
+    // Müşteri güncelleme işlemi
+    public void update() {
+        System.out.println("Müşteri güncellendi.");
+    }
+}
+
